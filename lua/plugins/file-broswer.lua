53eddf21b70f01376afcaf854b1a602a747d91ee
samofoke <sabataernestmofokeng@gmail.com>
2025-02-07 23:26:57 +0200

    expanded Telescope

--- lua/plugins/file-broswer.lua
+++ lua/plugins/file-broswer.lua
@@ -2,28 +2,134 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8", -- or branch = "0.1.x"
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "Telescope", -- loads Telescope only when a Telescope command is used
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          -- add your preferred defaults here
-          path_display = { "smart" },
-        },
-      })
-    end,
-  },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
+    priority = 1000,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
     keys = {
+      {
+        ";f",
+        function()
+          require("telescope.builtin").find_files({
+            no_ignore = false,
+            hidden = true,
+          })
+        end,
+        desc = "Lists files in your current working directory, respects .gitignore",
+      },
+      {
+        ";r",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Search for a string in your cwd live, respects .gitignore",
+      },
+      {
+        "\\\\",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "Lists open buffers",
+      },
+      {
+        ";;",
+        function()
+          require("telescope.builtin").resume()
+        end,
+        desc = "Resume the previous telescope picker",
+      },
+      {
+        ";e",
+        function()
+          require("telescope.builtin").diagnostics()
+        end,
+        desc = "Lists diagnostics for buffers",
+      },
+      {
+        ";s",
+        function()
+          require("telescope.builtin").treesitter()
+        end,
+        desc = "Lists Treesitter symbols (functions, variables, etc.)",
+      },
+      {
+        "sf",
+        function()
+          local telescope = require("telescope")
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open file browser with the path of the current buffer",
+      },
       {
         "<leader>sB",
         "<cmd>Telescope file_browser path=%:p:h<CR>",
         desc = "Browse Files",
       },
     },
-    config = function()
-      require("telescope").load_extension("file_browser")
+    config = function(_, opts)
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local fb_actions = telescope.extensions.file_browser.actions
+
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        wrap_results = true,
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+        mappings = {
+          n = {},
+        },
+      })
+      opts.pickers = {
+        diagnostics = {
+          theme = "ivy",
+          initial_mode = "normal",
+          layout_config = { preview_cutoff = 9999 },
+        },
+      }
+      opts.extensions = {
+        file_browser = {
+          theme = "dropdown",
+          hijack_netrw = true,
+          mappings = {
+            ["n"] = {
+              ["N"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["<C-u>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_previous(prompt_bufnr)
+                end
+              end,
+              ["<C-d>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_next(prompt_bufnr)
+                end
+              end,
+            },
+          },
+        },
+      }
+      telescope.setup(opts)
+      telescope.load_extension("fzf")
+      telescope.load_extension("file_browser")
     end,
   },
 }
